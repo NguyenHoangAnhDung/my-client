f1b9e5f03ea0dcf44ea397da5be4684293163d5a
nguyen.h.a.dung <FRAMGIA\nguyen.h.a.dung@b120645-lt.framgia.com>
2018-04-05 10:29:08 +0700

    Add Angular2Token to authorize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { MaterializeModule } from 'angular2-materialize';
+import { Angular2TokenService } from 'angular2-token';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    MaterializeModule
   ],
-  providers: [],
+  providers: [
+    Angular2TokenService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

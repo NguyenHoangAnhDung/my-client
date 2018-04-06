93c721134f3ac626160f39047e88fb295efe2ffa
nguyen.h.a.dung <FRAMGIA\nguyen.h.a.dung@b120645-lt.framgia.com>
2018-04-06 08:03:43 +0700

    [Issue 02 - part 01] Add view and authToken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+import { MaterializeModule } from 'angular2-materialize';
+import { Angular2TokenService } from 'angular2-token';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    Angular2TokenService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

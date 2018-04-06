171cade8d87690a8fca79fee53c9aa2c94b5bea5
nguyen.h.a.dung <FRAMGIA\nguyen.h.a.dung@b120645-lt.framgia.com>
2018-04-06 10:50:27 +0700

    [Issue 02 - part 02] Add open modal

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthDialogComponent } from '../auth-dialog/auth-dialog.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+  @ViewChild('authDialog') authDialog: AuthDialogComponent;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  presentAuthDialog(mode: string = 'login'): void {
+    this.authDialog.openDialog(mode);
   }
 
 }

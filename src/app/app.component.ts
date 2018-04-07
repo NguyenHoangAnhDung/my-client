3e2f77a75cef5ee4156b295763c177236568ef78
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2018-04-07 09:01:27 +0700

    Add condition to display Login/Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Angular2TokenService } from 'angular2-token';
-import { environment } from '../environments/environment';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +8,5 @@ import { environment } from '../environments/environment';
 export class AppComponent {
   title = 'app';
 
-  constructor(private authToken: Angular2TokenService) {
-    this.authToken.init(environment.token_auth_config);
-    this.authToken.signIn({ email: "user@gmail.com", password: "password123" })
-      .subscribe(res => console.log(res), err => console.log(err));
-  }
+  constructor() {}
 }
